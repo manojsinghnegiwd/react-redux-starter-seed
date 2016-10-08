547202d079998b59fb2b21e2ba882a796bada749
Manoj Singh Negi <justanothermanoj@gmail.com>
2016-10-08 18:09:15 +0530

    added index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {List, Map} from 'immutable';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import {AppContainer} from './components/App';
+import {compose, createStore} from 'redux';
+
+const createStoreDevTools = compose(
+	window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore);
+
+const store = createStoreDevTools(reducer);
+
+store.dispatch({
+	type: 'SET_STATE',
+	state: {
+		message: 'Enjoy Immutable , React and redux'
+	}
+});
+
+ReactDOM.render(
+	<Provider store={store}>
+		<AppContainer />
+	</Provider>,
+	document.getElementById('app')
+);

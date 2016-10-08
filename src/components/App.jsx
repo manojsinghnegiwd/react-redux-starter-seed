eee2ed7109bc6b1a267f89b817c1a6cf8a7c30e0
Manoj Singh Negi <justanothermanoj@gmail.com>
2016-10-08 20:05:45 +0530

    refactored component into smaller components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import {connect} from 'react-redux';
-
+import Text from './Text';
 import * as actionCreators from '../action_creators';
 
 export class App extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this._changeColor = this._changeColor.bind(this);
-	}
-
-	_changeColor () {
-		return this.props.changeColor('red');
-	}
 
     render() {
         return (
-        	<h1 style={{color: this.props.textColor}} onClick={this._changeColor}>{this.props.message}</h1>
+        	<Text {...this.props} />
         );
     }
 }

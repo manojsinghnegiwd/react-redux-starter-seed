0d29188745b89994853b8ad3d46cd014cc29bc61
Manoj Singh Negi <justanothermanoj@gmail.com>
2016-10-08 19:37:43 +0530

    added function to handle CHANGE_COLOR

--- src/reducer.js
+++ src/reducer.js
@@ -4,9 +4,15 @@ function setState(state, newState) {
 	return state.merge(newState);
 }
 
+function changeColor(state, color) {
+	return state.set('textColor', color);
+}
+
 export default function (state = Map(), action) {
 	switch(action.type) {
 		case 'SET_STATE':
 			return setState(state, action.state);
+		case 'CHANGE_COLOR':
+			return changeColor(state, action.color);
 	}
 }

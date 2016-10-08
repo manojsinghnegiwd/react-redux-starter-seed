58677849ccea740c1faaee0e90070f1225fa82ed
Manoj Singh Negi <justanothermanoj@gmail.com>
2016-10-08 19:57:51 +0530

    added text component

---
+++ src/components/Text.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export class Text extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this._changeColor = this._changeColor.bind(this);
+	}
+
+	_changeColor () {
+		return this.props.changeColor('red');
+	}
+
+    render() {
+        return (
+        	<h1 style={{color: this.props.textColor}} onClick={this._changeColor}>{this.props.message}</h1>
+        );
+    }
+}
